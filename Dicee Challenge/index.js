1af87c4e8876ca84aa1737854bdb42d882dbe1a9
Dejun Jackson <62167185+DejunJackson@users.noreply.github.com>
2021-05-07 14:07:54 -0500

    Update index.js

--- Dicee Challenge/index.js
+++ Dicee Challenge/index.js
@@ -1,39 +1,25 @@
+//Assigns random number (1-6) to Player 1
 var randomNumber1 = Math.random();
 randomNumber1 = Math.floor(randomNumber1 * 6) + 1;
-if (randomNumber1 === 1){
-  document.querySelector("img.img1").setAttribute("src", "images/dice1.png");
-}else if (randomNumber1 === 2){
-  document.querySelector("img.img1").setAttribute("src", "images/dice2.png");
-}else if (randomNumber1 === 3){
-  document.querySelector("img.img1").setAttribute("src", "images/dice3.png");
-}else if (randomNumber1 === 4){
-  document.querySelector("img.img1").setAttribute("src", "images/dice4.png");
-}else if (randomNumber1 === 5){
-  document.querySelector("img.img1").setAttribute("src", "images/dice5.png");
-}else if (randomNumber1 === 6){
-  document.querySelector("img.img1").setAttribute("src", "images/dice6.png");
-}
 
+
+//Attaches the right image to whatever number is randomly picked for Player 1
+var randomDiceImage = "images/dice" + randomNumber1 + ".png";
+document.querySelector("img.img1").setAttribute("src", randomDiceImage);
+
+
+//Does the same thing for Player 2
 var randomNumber2 = Math.random();
 randomNumber2 = Math.floor(randomNumber2 * 6) + 1;
-if (randomNumber2 === 1){
-  document.querySelector("img.img2").setAttribute("src", "images/dice1.png");
-}else if (randomNumber2 === 2){
-  document.querySelector("img.img2").setAttribute("src", "images/dice2.png");
-}else if (randomNumber2 === 3){
-  document.querySelector("img.img2").setAttribute("src", "images/dice3.png");
-}else if (randomNumber2 === 4){
-  document.querySelector("img.img2").setAttribute("src", "images/dice4.png");
-}else if (randomNumber2 === 5){
-  document.querySelector("img.img2").setAttribute("src", "images/dice5.png");
-}else if (randomNumber2 === 6){
-  document.querySelector("img.img2").setAttribute("src", "images/dice6.png");
-}
+var randomDiceImage2 = "images/dice" + randomNumber2 + ".png";
+document.querySelector("img.img2").setAttribute("src", randomDiceImage2);
 
+
+//Compares scores and determines winner
 if (randomNumber1 > randomNumber2){
-  document.querySelector("h1").innerHTML = "Player 1 Wins!";
+  document.querySelector("h1").innerHTML = "🚩Player 1 Wins!";
 }else if (randomNumber1 < randomNumber2){
-  document.querySelector("h1").innerHTML = "Player 2 Wins!";
+  document.querySelector("h1").innerHTML = "🚩Player 2 Wins!";
 }else if (randomNumber1 === randomNumber2){
-  document.querySelector("h1").innerHTML = "Draw!";
+  document.querySelector("h1").innerHTML = "⚔️Draw!";
 }
